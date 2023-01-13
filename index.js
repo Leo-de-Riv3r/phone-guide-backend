8f5c840cfd51b7c9541a6287c3a64b50feb5c6f5
Leo-de-Riv3r <leoguti182@gmail.com>
2023-01-13 13:47:16 -0300

    backend

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ const unknownEndpoint = (req, res) => {
 const errorHandler = (error, req, res, next) => {
   console.error(error.message)
 
-  if (error.name == 'CastError') {
+  if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' })
-  } else if (error.name == 'ValidationError') {
+  } else if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message })
   }
 
